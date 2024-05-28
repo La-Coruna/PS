27c16f12eda27a81907d352671c1ab6296c3c0bd
Lacoruna <65222636+La-Coruna@users.noreply.github.com>
2024-05-28 15:12:42 +0900

    Create 1919.cpp

---
+++ baekjoon/1919.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int alphabet[26] = {0};
+    string str1, str2;
+    int answer = 0;
+
+    cin >> str1 >> str2;
+
+    for(auto c: str1){
+        alphabet[c-'a']++;
+    }
+    for(auto c: str2){
+        alphabet[c-'a']--;
+    }
+    for(auto i: alphabet){
+        if(i!=0){
+            answer += abs(i);
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
